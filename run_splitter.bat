040d64a7a73c5095c68442382328963b4ea1ff02
kshitizgupta-nt <kshitiz.gupta@nucleusteq.com>
2026-09-20 20:36:52 +0530

    Delete run_splitter.bat

--- run_splitter.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set "HADOOP_HOME=%USERPROFILE%\hadoop"
-set "hadoop.home.dir=%HADOOP_HOME%"
-python data_ingress\file_splitter.py data_ingress\demo_data.jsonl data_ingress\split_chunks 3
-if errorlevel 1 exit /b 1
-echo PySpark split completed.
